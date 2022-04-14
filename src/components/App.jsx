030ad36afff7249ccc0610020a4ea5b1d29cc7d6
simoneya <yavar.simona@yahoo.com>
2022-04-14 10:25:19 +0100

    components update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import Card from "./Card.jsx";
+import Card from "./Card";
 import contacts from "../contacts";
+import Avatar from "./Avatar";
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
+      <Avatar img="https://media.self.com/photos/5ff4d8addc4f8827459c38a9/4:3/w_3783,h_2837,c_limit/Self_Ciara_6.jpg" />
       <Card
         name={contacts[0].name}
         img={contacts[0].imgURL}
